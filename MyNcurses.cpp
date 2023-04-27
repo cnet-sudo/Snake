4adcf4e6290776c8a629835a7785d0c23a11083f
cnet-sudo <cnetcnetcnet3@gmail.com>
2023-04-27 23:39:33 +0300

    27 04 23

--- MyNcurses.cpp
+++ MyNcurses.cpp
@@ -8,39 +8,46 @@
 using namespace std;
 using namespace std::chrono;
 
-struct vector2di{
+struct vector2di {
 
-    int x=0;
-    int y=0;
+    int x = 0;
+
+    int y = 0;
 };
 
-void move_snake(vector2di &snake_head, const vector2di & vector_step,vector<vector2di> & snake_tail, 
-    vector2di& apple,vector2di rnd){
+void move_snake(vector2di & snake_head, const vector2di & vector_step,vector<vector2di> & snake_tail, 
+    vector2di & apple, vector2di rnd) {
 
     // устанавливаем цвет змеи
     attrset(A_BOLD | COLOR_PAIR(3));
 
     // рисуем хвост змеи если он есть
-    if (!snake_tail.empty()){
+    if (!snake_tail.empty()) {
 
-        for (auto const & mov : snake_tail){
+        for (auto const & mov : snake_tail) {
 
-            move(mov.y, mov.x);
+            move(mov.y, mov.x); 
+
             addch('#');
         }
      }
     // изменяем координаты головы змеи
     snake_head.x += vector_step.x;
+
     snake_head.y += vector_step.y;
+
     move(snake_head.y, snake_head.x);
+
     // проверяем символ в установленных координатах курсора
     auto s = static_cast<char>(winch(stdscr));
+
     if (s == '!' || s=='#') exit(0);
+
     // змея съедает яблоко
-    if (s == '@'){
+    if (s == '@') {
 
         // добавляем хвост змеи
-        snake_tail.push_back({snake_head.x, snake_head.y});
+        snake_tail.push_back( {snake_head.x, snake_head.y});
         // рисуем голову змеи
         addch('$'); 
 
@@ -48,40 +55,45 @@ void move_snake(vector2di &snake_head, const vector2di & vector_step,vector<vect
 
         // новые координаты яблока
         apple.x = rnd.x; apple.y = rnd.y;
+
         move(apple.y, apple.x);
+
         s = static_cast<char>(winch(stdscr));
         // повторяем пока координаты яблока совпадают с хвостом змеи
         } while (s == '#'|| s == '$');
     }
-    else{
+    else {
 
         // рисуем голову змеи
         addch('$');
         // обновляем координаты хвоста змеи
-        if (!snake_tail.empty()){
+        if (!snake_tail.empty()) {
 
-            snake_tail.push_back({ snake_head.x, snake_head.y });
+            snake_tail.push_back( { snake_head.x, snake_head.y });
+
             snake_tail.erase(snake_tail.begin());       
         }
     }
     refresh(); 
 };
 
-void show_map(vector2di & apple, vector2di rnd){
+void show_map(vector2di & apple, vector2di rnd) {
 
     // очистка экрана
     clear();
     // перемещение курсора
     move(1,50);
+
     attrset(A_DIM | COLOR_PAIR(1));
+
     printw("Змейка\n"); 
 
     // рисуем игровое поле 
-    for (int i = 0; i < 25; i++){   
+    for (int i = 0; i < 25; i++) {   
         
         move(i+3, 5);
 
-        for (int j = 0; j < 100; j++){
+        for (int j = 0; j < 100; j++) {
 
             if (i == 0 || i == 24) addch('!');
             else 
@@ -91,14 +103,17 @@ void show_map(vector2di & apple, vector2di rnd){
 
         addch('\n');
     }
-    if (apple.x == 0){ 
+    if (apple.x == 0) { 
         
         apple.x = rnd.x; apple.y = rnd.y;
     }
     // рисуем яблоко
     move(apple.y, apple.x);
+
     attrset(A_BOLD | COLOR_PAIR(2));
+
     addch('@');
+
     refresh();    
 }
 
@@ -121,11 +136,12 @@ int main(){
     //цвет фона
     bkgd(COLOR_PAIR(1));
     // начальные координаты головы
-    vector2di snake_head{10,10};
+    vector2di snake_head  {10,10};
     // направление и шаг перемещения
-    vector2di vector_step{1,0};
+    vector2di vector_step  {1,0};
     // координаты яблока
     vector2di apple;
+
     vector2di rnd_apple;
     // координаты хвоста змеи 
     vector<vector2di> snake_tail;
@@ -139,7 +155,7 @@ int main(){
     int pause = 100;
 
     // игровой цикл
-    while (true){
+    while (true) {
 
     rnd_apple.x = apple_x(rnd);
     rnd_apple.y = apple_y(rnd);
@@ -152,10 +168,10 @@ int main(){
     // разрешаем использовать специальные клавиши в нашем случае стрелки    
     keypad(stdscr, true);
     // проверка нажатой клавиши
-    switch (getch()){
+    switch (getch()) {
 
     case KEY_UP:
-        if (vector_step.y == 0){ 
+        if (vector_step.y == 0) { 
         
         vector_step.y = -1; 
         vector_step.x = 0; 
@@ -164,7 +180,7 @@ int main(){
         break;
 
     case KEY_DOWN:
-        if (vector_step.y == 0){ 
+        if (vector_step.y == 0) { 
             
             vector_step.y = 1; 
             vector_step.x = 0; 
@@ -172,7 +188,7 @@ int main(){
         break;
 
     case KEY_LEFT:  
-        if (vector_step.x == 0){ 
+        if (vector_step.x == 0) { 
             
             vector_step.x = -1; 
             vector_step.y = 0; 
@@ -180,7 +196,7 @@ int main(){
         break;
 
     case KEY_RIGHT:   
-        if (vector_step.x == 0){ 
+        if (vector_step.x == 0) { 
             
             vector_step.x = 1; 
             vector_step.y = 0; 
